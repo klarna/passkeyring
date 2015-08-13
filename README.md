229374256bcbc83c9f38d7c579b669c887c81a29
Ozan Safi <ozan.safi@klarna.com>
2015-08-13 22:08:41 +0200

    Initial commit
    Add in the source code for passkeyring
    and an accompanying README.
    
    Signed-off-by: Ozan Safi <ozan.safi@klarna.com>

---
+++ README.md
@@ -0,0 +1,27 @@
+passkeyring
+===========
+A python-keyring backend for the 'pass' password manager.
+
+How to set passkeyring as the default keyring backend?
+------------------------------------------------------
+
+First find the platform-specific directory location to place the configuration file:
+
+```
+config_root=$(python -c "import keyring.util.platform_; print(keyring.util.platform_.config_root())")
+
+echo "[backend]
+default-keyring=passkeyring.PassKeyring
+keyring-path=<path_to_module_dir>" > $config_root/keyringrc.cfg
+```
+
+How to set passkeyring as the keyring to use at runtime?
+--------------------------------------------------------
+```
+import keyring
+from passkeyring import PassKeyring
+
+keyring.set_keyring(PassKeyring())
+```
+
+And the rest is the same as usual.
---
+++ passkeyring.py
@@ -0,0 +1,33 @@
+import keyring.backend
+
+import os
+from subprocess import Popen, PIPE
+
+class PassKeyring(keyring.backend.KeyringBackend):
+    """A keyring interfacing with Pass
+    """
+    priority = 1
+
+    def set_password(self, servicename, username, password):
+        p1 = Popen(["echo", password], stdout=PIPE)
+        p2 = Popen(["pass", "insert", "-e", "-f", servicename+"/"+username], stdin=p1.stdout, stdout=PIPE)
+        p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+        (stdout, stderr) = p2.communicate()
+        exit_code = p2.wait()
+        return exit_code
+
+    def get_password(self, servicename, username):
+        process = Popen(["pass", servicename+"/"+username], stdout=PIPE)
+        (stdout, stderr) = process.communicate()
+        exit_code = process.wait()
+        if exit_code == 0:
+            return stdout.rstrip()
+        else:
+            return exit_code
+
+    def delete_password(self, servicename, username):
+        process = Popen(["pass", "rm", "-f", servicename+"/"+username], stdout=PIPE)
+        (stdout, stderr) = process.communicate()
+        exit_code = process.wait()
+        return exit_code
+
